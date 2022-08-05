0efb1f8f1b298606d99f065f27d41b41537ab49a
Pablo Carneiro Elias <pablo.cael@gmail.com>
2022-08-05 09:57:32 +0900

    Added disassembly tests

---
+++ disassembly/test.cpp
@@ -0,0 +1,9 @@
+
+int main(void) {
+    float x = 0;
+    float y = 0;
+
+    float z = x + y;
+    float w = z + 3;
+    return 0;
+}
---
+++ disassembly/test2.cpp
@@ -0,0 +1,12 @@
+int main(void) {
+
+    int a = 1;
+    int b = 0;
+    int c = 0;
+    if( a == 1 || b == 0 ) {
+       c = 1;
+    }
+    else {
+        c = 2;
+    }
+}
